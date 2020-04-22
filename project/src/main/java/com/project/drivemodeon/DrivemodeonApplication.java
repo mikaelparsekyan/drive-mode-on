5f51b63b7b87cb55bc968c96593a1572c36cf39a
speeddemon <mikael90@abv.bg>
2020-04-22 16:23:10 +0300

    Initial commit

---
+++ project/src/main/java/com/project/drivemodeon/DrivemodeonApplication.java
@@ -0,0 +1,13 @@
+package com.project.drivemodeon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrivemodeonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DrivemodeonApplication.class, args);
+    }
+
+}
---
+++ project/src/test/java/com/project/drivemodeon/DrivemodeonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.drivemodeon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrivemodeonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
