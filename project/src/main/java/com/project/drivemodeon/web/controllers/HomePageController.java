09b82cab089509f3943e4a807e3c6ce4cf61e93c
speeddemon <mikael90@abv.bg>
2020-04-22 21:24:08 +0300

    Added simple controllers

---
+++ project/src/main/java/com/project/drivemodeon/web/controllers/HomePageController.java
@@ -0,0 +1,15 @@
+package com.project.drivemodeon.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomePageController {
+
+    @GetMapping(value = {"/", "/index", "/home"})
+    public ModelAndView getHomePage(ModelAndView modelAndView) {
+        modelAndView.setViewName("index");
+        return modelAndView;
+    }
+}
---
+++ project/src/main/java/com/project/drivemodeon/web/controllers/RegisterPageController.java
@@ -0,0 +1,7 @@
+package com.project.drivemodeon.web.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class RegisterPageController {
+}
