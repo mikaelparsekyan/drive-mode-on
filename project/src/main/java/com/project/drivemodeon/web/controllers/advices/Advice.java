b065087c1bd5e7908ba68326bb9e3b483c6f0dd9
speeddemon <mikael90@abv.bg>
2020-05-06 17:46:07 +0300

    Added username in profile view

--- project/src/main/java/com/project/drivemodeon/web/controllers/advices/Advice.java
+++ project/src/main/java/com/project/drivemodeon/web/controllers/advices/Advice.java
@@ -1,18 +1,39 @@
 package com.project.drivemodeon.web.controllers.advices;
 
+import com.project.drivemodeon.domain.models.User;
+import com.project.drivemodeon.services.api.UserService;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Optional;
 
 @ControllerAdvice
 public class Advice {
+    private final UserService userService;
 
+    public Advice(UserService userService) {
+        this.userService = userService;
+    }
 
     @ModelAttribute("isUserLogged")
     public boolean isUserLoggedIn(HttpServletRequest request) {
         Long loggedUserId = (Long) request.getSession().getAttribute("user_id");
 
         return loggedUserId != null;
+    }
+
+    @ModelAttribute("username")
+    public String getUsername(HttpServletRequest request) {
+        Long loggedUserId = (Long) request.getSession().getAttribute("user_id");
+
+        if (loggedUserId != null) {
+            Optional<User> user = userService.getUserById(loggedUserId);
+            if (user.isPresent()) {
+                return user.get().getUsername();
+            }
+        }
+
+        return null;
     }
 }
