9c22433cab8ad0548992b930cbd7c50dfc4fb1c5
speeddemon <mikael90@abv.bg>
2020-05-28 16:13:32 +0300

    Added user dropdown menu (frontend)

---
+++ project/src/main/resources/static/js/script.js
@@ -0,0 +1,6 @@
+function showUserProfileDropdown() {
+    var userDropdown = $('.user-dropdown');
+    userDropdown.addClass('active');
+    var dropdownArrow = $('.dropdown-arrow');
+    dropdownArrow.addClass('active');
+}
