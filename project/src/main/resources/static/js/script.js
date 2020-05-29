174a8b1867cebb491b531095bceca81d9ce20e09
speeddemon <mikael90@abv.bg>
2020-05-29 18:41:42 +0300

    Fixed minor styling issues

--- project/src/main/resources/static/js/script.js
+++ project/src/main/resources/static/js/script.js
@@ -1,11 +1,20 @@
+function deactivateDimming() {
+    deactivateAllPopups();
+    $('#dimming').addClass('deactivated');
+}
+
 function toggleUserDropdownMenu() {
-    var userDropdown = $('.user-dropdown');
-    var dropdownArrow = $('.dropdown-arrow');
-    var activeClass = ('active');
+    let userDropdown = $('.user-dropdown');
+    let dropdownArrow = $('.dropdown-arrow');
+    let activeClass = ('active');
 
     userDropdown.hasClass(activeClass) ? userDropdown.removeClass(activeClass) :
         userDropdown.addClass(activeClass);
 
     dropdownArrow.hasClass(activeClass) ? dropdownArrow.removeClass(activeClass) :
         dropdownArrow.addClass(activeClass);
+}
+
+function showUserLogoutPopup() {
+    $('#dimming').removeClass('deactivated');
 }
