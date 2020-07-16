e60f34ea9244b94e025c8460b493fe1d61476fa4
speeddemon <mikael90@abv.bg>
2020-07-16 09:46:00 +0300

    Added draft posting functionality

--- project/src/main/java/com/project/drivemodeon/web/controller/DraftController.java
+++ project/src/main/java/com/project/drivemodeon/web/controller/DraftController.java
@@ -1,5 +1,6 @@
 package com.project.drivemodeon.web.controller;
 
+import com.google.gson.Gson;
 import com.project.drivemodeon.model.entity.User;
 import com.project.drivemodeon.model.service.user.UserServiceModel;
 import com.project.drivemodeon.service.api.post.PostService;
@@ -7,28 +8,31 @@ import com.project.drivemodeon.service.api.user.UserService;
 import org.modelmapper.ModelMapper;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.security.Principal;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
-@RequestMapping("/drafts")
+@RequestMapping("/draft")
 public class DraftController {
     private final PostService postService;
     private final UserService userService;
     private final ModelMapper modelMapper;
+    private final Gson gson;
 
-    public DraftController(PostService postService, UserService userService, ModelMapper modelMapper) {
+    public DraftController(PostService postService, UserService userService, ModelMapper modelMapper, Gson gson) {
         this.postService = postService;
         this.userService = userService;
         this.modelMapper = modelMapper;
+        this.gson = gson;
     }
 
     @GetMapping
     public ModelAndView getDraftsPage(@AuthenticationPrincipal Principal principal) {
-        ModelAndView modelAndView = new ModelAndView("drafts");
+        ModelAndView modelAndView = new ModelAndView("draft");
         if (principal == null) {
             modelAndView.setViewName("redirect:/");
             return modelAndView;
@@ -46,5 +50,21 @@ public class DraftController {
                 )
         );
         return modelAndView;
+    }
+
+    @PostMapping("/add/{id}")
+    @ResponseBody
+    public String addDraftAsPost(@PathVariable("id") String strId) {
+        Map<String, Object> jsonResult = new HashMap<>();
+        jsonResult.put("success", false);
+
+        try {
+            long id = Long.parseLong(strId);
+            System.out.println(id);
+        } catch (Exception e) {
+
+        }
+
+        return gson.toJson(jsonResult);
     }
 }
---
+++ project/src/main/java/com/project/drivemodeon/web/controller/advice/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.project.drivemodeon.web.controller.advice;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.NoHandlerFoundException;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public ModelAndView handleNotFoundError() {
+        ModelAndView modelAndView = new ModelAndView();
+
+        return modelAndView;
+    }
+
+}
